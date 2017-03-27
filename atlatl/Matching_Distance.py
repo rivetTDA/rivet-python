9f4cdd74cbd4c03f657501caeddb5aedc941c2be
Michael Lesnick <mlesnick@nat-oitwireless-inside-vapornet100-d-31697.Princeton.EDU>
2017-03-27 16:44:37 -0400

    Small changes.

--- atlatl/Matching_Distance.py
+++ atlatl/Matching_Distance.py
@@ -32,17 +32,13 @@ def matching_distance(Mod1,Mod2,Grid_Parameter,Normalize,Fixed_Bounds):
     #Mod1,Mod2: RIVET "precomputed" representations of two persistence modules, in Bryn's python bytes format   
     
     #Grid_Parameter: This is a non-negative integer which should be at least 1.
-    #We will choose Grid_parameter slopes between 0 and 90; we do not however consider the values 0 and 90, since
-    #these are not considered in the definition of the matching distance.  
-    #We will also choose Grid_parameter offset values, for each slope, in such a way that the
-    #largest and smallest offsets specify lines that touch the upper left and lower right corners of the rectangular 
-    #region of interest.
+    #We will choose Grid_Parameter values of slope and also choose Grid_Parameter offset values, for each slope.
     
     #Normalize: Boolean; True iff we compute the distances with constants chosen to simulate the situation where
     # the coordinates are rescaled so that UR-LL=[1,1]?
     
     #Fixed_Bounds is a list.  
-    #It can be empty.  Alternatively, bounds can be a pair of numbers [LL,UR]
+    #It can be empty.  Alternatively, Fixed_Bounds can be a pair of numbers [LL,UR]
     #which specifies the bounds to work with.  
     #The purpose of this latter option is to allow the user to compute matching distances with 
     #uniform precision over a large collection of 2-D persistence modules, which may exhibit features at different scales.
@@ -67,17 +63,26 @@ def matching_distance(Mod1,Mod2,Grid_Parameter,Normalize,Fixed_Bounds):
     List_Of_Lines=[];
     
     for i in range(Grid_Parameter):
+        #We will choose Grid_parameter slopes between 0 and 90; 
+        #we do not however consider the values 0 and 90, since in view of stability considerations
+        #these are not considered in the definition of the matching distance.  
         sl=90*(i+1)/(Grid_Parameter+1)
-    
+        
         #find the offset parameters such that the lines with slope sl just touches the upper left corner of the box
         UL=[LL[0],UR[1]]
         UL_Offset=find_offset(sl,UL)
         LR=[UR[0],LL[1]]
         LR_Offset=find_offset(sl,LR)
         
-        for j in range(Grid_Parameter):
-            offset=LR_Offset+(j+1)*(UL_Offset-LR_Offset)/(Grid_Parameter+1)    
-            List_Of_Lines.append((sl,offset))
+        #Choose the values of offset for this particular choice of sl.
+        if Grid_Parameter==1:
+            List_Of_Lines.append((sl,UL_Offset-LR_Offset))
+        #largest and smallest offsets specify lines that touch 
+        #the upper left and lower right corners of the rectangular region of interest.
+        else:
+            for j in range(Grid_Parameter):
+                offset=LR_Offset+j*(UL_Offset-LR_Offset)/(Grid_Parameter-1)    
+                List_Of_Lines.append((sl,offset))
 
     #next, for each of the two 2-D persistence modules, get the barcode associated to the list of lines. 
     multi_bars1=rivet.barcodes(Mod1,List_Of_Lines)
@@ -92,7 +97,7 @@ def matching_distance(Mod1,Mod2,Grid_Parameter,Normalize,Fixed_Bounds):
                        
         #To determine the weight to use for the line given by (sl,offset), we need to take into account both
         #the weight coming from slope of the line, and also the normalization, which changes both the effective 
-        #weight and the effective bottleneck distance.  
+        #weight and the effective bottleneck distance.
             
         #first, let's recall how the reweighting works in the unnormalized case.  According to the 
         #definition of the matching distance, we choose 
@@ -127,8 +132,7 @@ def matching_distance(Mod1,Mod2,Grid_Parameter,Normalize,Fixed_Bounds):
             #of course, this code can be made more compact, but hopfully this way is readible    
                 
             #moreover, normalizaion changes the length of a line segment along the line (sl,offset), 
-            #and hence also the bottleneck distance, by a factor of 
-            
+            #and hence also the bottleneck distance, by a factor of             
             bottleneck_stretch=np.sqrt(((m/delta_y)**2+(1/delta_x)**2)/(m**2+1))
             m_dist=max(m_dist,w*raw_distance*bottleneck_stretch)
 
